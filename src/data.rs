19691004c96f2fe5bb1e3f2197d1ef72a5c01be3
PRC <105520102+x0prc@users.noreply.github.com>
2025-04-11 00:58:15 +0530

    feat: WIP img support

--- src/data.rs
+++ src/data.rs
@@ -1,8 +1,19 @@
 //! the main engine
+
+#[cfg(feature = "image-to-ascii")]
 use std::io;
 
-use image::io::Reader as ImageReader;
+#[cfg(feature = "image-to-ascii")]
 use rascii_art::render_image;
+
+#[cfg(feature = "image-to-ascii")]
+use image::io::Reader as ImageReader024;
+
+#[cfg(feature = "image")]
+use image_025::ImageReader as ImageReader025;
+
+#[cfg(feature = "image")]
+use viuer::print;
 
 use crate::config_handler::Config;
 use crate::util::path_utils::get_path;
@@ -29,13 +40,14 @@ impl Data {
         }
     }
 
-    /// If image configuration is provided, render image to stdout
+    /// If image configuration is provided, render image as ascii to stdout
     ///
     /// NOTE: uses [`rascii_art::render_image`]
-    pub fn render_image(&self) {
+    #[cfg(feature = "image-to-ascii")]
+    pub fn render_image_as_ascii(&self) {
         if let Some(image_config) = &self.config.image {
             let image_path = get_path(&image_config.path);
-            let image = ImageReader::open(image_path)
+            let image = ImageReader024::open(image_path)
                 .unwrap()
                 .with_guessed_format()
                 .unwrap()
@@ -49,6 +61,25 @@ impl Data {
             render_image(&image, &mut buffer, &render_options).unwrap();
         } else {
             eprintln!("No image config found. Skipping image rendering.");
+        }
+    }
+
+    /// If image configuration is provided, render image to stdout
+    ///
+    /// NOTE: uses [`viuer::print`]
+    #[cfg(feature = "image")]
+    #[allow(dead_code)]
+    pub fn render_image(&self) {
+        if let Some(image_config) = &self.config.image {
+            let viuer_config = image_config.get_viuer_config();
+            let image_path = get_path(&image_config.path);
+            let image = ImageReader025::open(image_path)
+                .unwrap()
+                .with_guessed_format()
+                .unwrap()
+                .decode()
+                .unwrap();
+            print(&image, &viuer_config).expect("Image printing failed.");
         }
     }
 }
