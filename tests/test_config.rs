220cda85d2c99ae4f56438c9ba64d24acb4bd49a
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-07-19 18:15:05 +0100

    add CI workflow to write output as comment
    Co-authored-by: x0prc <c0ldheat@protonmail.com>

--- tests/test_config.rs
+++ tests/test_config.rs
@@ -5,7 +5,7 @@ use symfetch::config_handler::Config;
 #[test]
 fn test_config_init() {
     let config = Config::new(&PathBuf::from("tests/only_ascii.toml")).unwrap();
-    let test_ascii_path = PathBuf::from("ascii");
+    let test_ascii_path = PathBuf::from("tests/ascii");
 
     let ascii_config = config.ascii.as_ref().unwrap();
 
